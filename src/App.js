6e6138d5fa9fdb6453ed08559fa83699016981e7
coskun <coskun.4901@gmail.com>
2023-10-31 22:41:21 +0300

    deneme işlemler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useReducer } from 'react';
+import Header from './components/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 function App() {
+
+  function reducer(state,action){
+    switch (action.type) {
+      case 'SET_TODO':
+        return {
+          ...state,
+          todo:action.value
+        }
+
+        case 'ADD_TODO':
+          return {
+            ...state,
+            todo:'',
+            todos:[
+              ...state.todos,
+              action.todo
+           ]
+          }
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer,{
+    todos:[],
+    todo:''
+  })
+
+  const submitHandle = e => {
+    e.preventDefault()
+
+    dispatch({
+      type:'ADD_TODO',
+      todo:state.todo
+    })
+  }
+
+  const onChange = e => {
+    dispatch({
+      type:'SET_TODO',
+      value:e.target.value
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <h1>Todo App</h1>
+    <AddTodo submitHandle={submitHandle} onChange={onChange} todo = {state.todo} />
+    <Todos todos={state.todos} />
+    </>
   );
 }
 
